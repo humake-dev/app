036f7f95c3d2ec6a4913402045033b6ac5214452
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-31 04:01:09 +0900

    수정함:        App.tsx 	수정함:        Config.js

--- Config.js
+++ Config.js
@@ -1,3 +1,11 @@
 import Config from 'react-native-config';
 
-export const BASE_URL = `${Config.API_PROTOCOL}://${Config.API_HOST}:${Config.API_PORT}`;
+const API_PROTOCOL = Config.API_PROTOCOL ?? 'https';
+const API_HOST = Config.API_HOST ?? 'api.humake.co.kr';
+const API_PORT = Config.API_PORT ?? '443';
+
+if (!Config.API_PROTOCOL || !Config.API_HOST) {
+  console.warn('[ENV WARNING] .env not fully loaded', Config);
+}
+
+export const BASE_URL = `${API_PROTOCOL}://${API_HOST}:${API_PORT}`;
