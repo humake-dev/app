0d068506f28de0404937fcb126d27d62fd9de9b6
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-31 14:14:18 +0900

    수정

--- Config.js
+++ Config.js
@@ -1,7 +1,11 @@
-// import Config from 'react-native-config';
+import Config from 'react-native-config';
 
-const API_PROTOCOL ='https';
-const API_HOST = 'api.humake.co.kr';
-const API_PORT = '443';
+const API_PROTOCOL = Config.API_PROTOCOL ?? 'https';
+const API_HOST = Config.API_HOST ?? 'api.humake.co.kr';
+const API_PORT = Config.API_PORT ?? '443';
+
+if (!Config.API_PROTOCOL || !Config.API_HOST) {
+  console.warn('[ENV WARNING] .env not fully loaded', Config);
+}
 
 export const BASE_URL = `${API_PROTOCOL}://${API_HOST}:${API_PORT}`;
